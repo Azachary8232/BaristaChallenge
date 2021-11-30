ac87275d9772ef5cc9086a76a0bf4ab0b71836de
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-30 15:46:32 -0700

    Add Itesm to Order

--- TestOrder.java
+++ TestOrder.java
@@ -4,14 +4,34 @@ public class TestOrder{
 
     public static void main(String[] args){
 
-        Order order1 = new Order();
-        Order order2 = new Order();
-        
-        Order order3 = new Order("Brian");
-        Order order4 = new Order("Marsha");
-        Order order5 = new Order("Paul");
-        
-        System.out.printf("%s", order4.getOrderName());
 
+        Item item1 = new Item("mocha", 1.95);
+        Item item2 = new Item("latte", 1.50);
+        Item item3 = new Item("drip coffee", 0.75);
+        Item item4 = new Item("capuccino", 4.75);
+
+
+        Order order1 = new Order();
+        order1.addItem(item1);
+        order1.addItem(item2);
+
+        Order order2 = new Order();
+        order2.addItem(item2);
+        order2.addItem(item3);
+
+        Order order3 = new Order("Brian");
+        order3.addItem(item3);
+        order3.addItem(item4);
+
+        Order order4 = new Order("Marsha");
+        order4.addItem(item4);
+        order4.addItem(item1);
+
+        Order order5 = new Order("Paul");
+        order5.addItem(item1);
+        order5.addItem(item2);
+
+        System.out.printf("%s", order4.getOrderName());
+        System.out.printf("%s", order5.getOrderItems());
     }
 }
